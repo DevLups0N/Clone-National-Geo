46bca788d4c8952e8bda5d1aa71fa20083b1da4f
Aluno-Luis_Filipe <luisfilipe02martins@gmail.com>
2025-07-10 21:56:49 -0300

    Arquivos menuMobile.css e script.js e index.html

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,6 @@
 import initTab from "./modules/tabNavigation.js";
 import iniAccordion from "./modules/accordion.js";
+import initMenuMobile from "./modules/menuMobile.js";
+initMenuMobile();
 iniAccordion();
 initTab();
